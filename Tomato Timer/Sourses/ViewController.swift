f36af8eadd93770aa41cb7d87b533db71c4000eb
Vyacheslav Onuchin <sla651@mail.ru>
2023-01-08 21:15:33 +1000

    Added a Label and a Button. Configured the color extension.

--- Tomato Timer/Sourses/ViewController.swift
+++ Tomato Timer/Sourses/ViewController.swift
@@ -8,17 +8,44 @@
 import UIKit
 import SnapKit
 
-// MARK: - UI Elements
-
-let backgroundImage: UIImageView = {
-    let image = UIImageView(image: UIImage(named: "background"))
-    image.contentMode = .scaleAspectFill
-    return image
-}()
-
-// MARK: - LifeCycle
-
 class ViewController: UIViewController {
+
+    // MARK: - Constant
+
+    enum Static {
+        static let indent: CGFloat = 30
+        static let buttonWidth = 60
+        static let buttonHight = 60
+    }
+
+    // MARK: - UI Elements
+
+    private var isWorkTime = false
+    private var isStarted = false
+
+    private lazy var backgroundImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "background"))
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "25:00"
+        label.sizeToFit()
+        label.textColor = UIColor(hex: "#141991")
+        label.font = UIFont.systemFont(ofSize: 38, weight: UIFont.Weight.bold)
+        return label
+    }()
+
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(systemName: "play"), for: UIControl.State.normal)
+        button.tintColor = UIColor(hex: "#141991")
+        return button
+    }()
+
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +58,9 @@ class ViewController: UIViewController {
     // MARK: - Setups
 
     private func setupHierarchy() {
-        [backgroundImage].forEach {view.addSubview($0)}
+        [backgroundImage,
+        label,
+        button].forEach {view.addSubview($0)}
     }
 
     private func setupView() {
@@ -42,6 +71,18 @@ class ViewController: UIViewController {
 
         backgroundImage.snp.makeConstraints { make in
             make.right.left.top.bottom.equalToSuperview()
+        }
+
+        button.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalTo(Static.buttonHight)
+            make.width.equalTo(Static.buttonWidth)
+        }
+
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(button.snp.top).offset(-Static.indent)
         }
     }
 
